5dbb3ae28d9a36891866ecb1e2219c61ae29ca92
Wendelin Gerein <info@webdelin.de>
2019-02-20 18:15:02 +0100

    commit webaplication create

---
+++ controllers/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+/**
+ * Description of SiteController
+ *
+ * @author wendelin
+ */
+class SiteController extends \yii\web\Controller{
+    
+    public function actionIndex() {
+        
+        return 'SiteController';
+        
+    }
+
+}
