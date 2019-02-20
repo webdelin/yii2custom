397e618b51ebedde47ea85c4840187d90f6fffec
Wendelin Gerein <info@webdelin.de>
2019-02-20 17:59:23 +0100

    zweite commit webaplication create

---
+++ config/web.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+* Wendelin Gerein
+* Webdesign/Webentwicklung
+* https://www.webdelin.de
+*
+* user=Wendelin Gerein <info@webdelin.de>
+*/
+
+return [
+    'id' => 'affilibeans',
+    'basePath' => realpath( __DIR__ . '/../'),
+    
+];
--- web/index.php
+++ web/index.php
@@ -1,1 +1,6 @@
-<h1>hello wendelin</h1>
+<?php
+
+    require __DIR__ .  '/../vendor/yiisoft/yii2/Yii.php';
+    $config = require __DIR__ .  '/../config/web.php';
+    $yii = new yii\web\Application($config);
+    $yii = run();
